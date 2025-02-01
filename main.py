97fbe6acde098c2b4886e6d7c57b857fed4c0977
Savelev Maxim <vismanmark@yandex.ru>
2025-02-01 12:58:05 +0300

    check linters

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.views import get_info
 
 if __name__ == "__main__":
     get_info("2020-05-20 12:55:32")  # YYYY-MM-DD HH:MM:SS
-    with open('data/output.json', 'r', encoding='utf-8') as json_file:
+    with open("data/output.json", "r", encoding="utf-8") as json_file:
         operations = json.load(json_file)
 
-    print(operations)
+    # print(operations)
--- src/views.py
+++ src/views.py
@@ -2,17 +2,17 @@ import datetime
 import json
 import os
 
+import finnhub
+import pandas
 import pandas as pd
 import requests
 from dotenv import load_dotenv
-import finnhub
-
 
 load_dotenv()
 
 
 def get_info(date: str) -> None:
-    """ Принимает дату в формате: YYYY-MM-DD HH:MM:SS
+    """Принимает дату в формате: YYYY-MM-DD HH:MM:SS
 
     Возвращает JSON-ответ следующего формата:
     - Приветствие [Доброе утро/день/вечер/ночь] # OK
@@ -31,19 +31,20 @@ def get_info(date: str) -> None:
     stock_prices = get_stock_prices()  # получить стоимость акций из S&P500
 
     full_response = {
-        'greeting': greeting,
-        'cards': cards,
-        'top_transactions': top_transactions,
-        'currency_rates': currency_rates,
-        'stock_prices': stock_prices
+        "greeting": greeting,
+        "cards": cards,
+        "top_transactions": top_transactions,
+        "currency_rates": currency_rates,
+        "stock_prices": stock_prices,
     }
 
     # запись данных в json-файл
-    with open('data/output.json', 'w', encoding='utf-8') as json_file:
+    with open("data/output.json", "w", encoding="utf-8") as json_file:
         json.dump(full_response, json_file, indent=4, ensure_ascii=False)
 
 
-def get_greeting(date) -> str:
+def get_greeting(date: str) -> str:
+    """Возвращает приветствие в зависимости от времени суток"""
     date_obj = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     hour = date_obj.hour  # час
 
@@ -57,68 +58,72 @@ def get_greeting(date) -> str:
         return "Доброй ночи!"
 
 
-def get_cards_info(date) -> tuple:
-    """ Возвращает инфорцию о картах и о топ транзакциях за указаный период """
+def get_cards_info(date: str) -> tuple:
+    """Возвращает инфорцию о картах и о топ транзакциях за указаный период"""
     # Перевод промежутка отслеживания операций в datetime
     date_obj = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     date_start = date_obj.replace(year=date_obj.year, month=date_obj.month, day=1, hour=0, minute=0, second=0)
 
     # получение данных и перевод дат в datetime
-    operations = read_excel('data/operations.xlsx')
-    operations['Дата операции'] = pd.to_datetime(operations['Дата операции'], format='%d.%m.%Y %H:%M:%S')
+    operations = read_excel("data/operations.xlsx")
+    operations["Дата операции"] = pd.to_datetime(operations["Дата операции"], format="%d.%m.%Y %H:%M:%S")
 
     # фильтрация по искомому временному промежутку
-    date_operations = operations.loc[(operations['Дата операции'] >= date_start)
-                                     & (operations['Дата операции'] <= date_obj)]
+    date_operations = operations.loc[
+        (operations["Дата операции"] >= date_start) & (operations["Дата операции"] <= date_obj)
+    ]
 
     # Группировка данных по картам
-    grouped_by_cards = date_operations.groupby('Номер карты')
+    grouped_by_cards = date_operations.groupby("Номер карты")
 
     cards = []
     for card_number, group in grouped_by_cards:
-        cards.append({
-            'last_digits': card_number[1:],
-            'total_spent': sum(group['Сумма операции с округлением']),
-            'cashback': sum(group['Бонусы (включая кэшбэк)'])
-        })
+        cards.append(
+            {
+                "last_digits": card_number[1:],
+                "total_spent": sum(group["Сумма операции с округлением"]),
+                "cashback": sum(group["Бонусы (включая кэшбэк)"]),
+            }
+        )
 
-    top_five_transactions = date_operations.sort_values('Сумма операции с округлением', ascending=False).head()
+    top_five_transactions = date_operations.sort_values("Сумма операции с округлением", ascending=False).head()
 
     top_transactions = []
     for index, row in top_five_transactions.iterrows():
-        top_transactions.append({
-            'date': row['Дата платежа'],
-            'amount': row['Сумма операции с округлением'],
-            'category': row['Категория'],
-            'description': row['Описание']
-        })
+        top_transactions.append(
+            {
+                "date": row["Дата платежа"],
+                "amount": row["Сумма операции с округлением"],
+                "category": row["Категория"],
+                "description": row["Описание"],
+            }
+        )
 
     return cards, top_transactions
 
 
-def read_excel(path_to_file: str):
-    """ Возвращает содержимое Excel - файла """
+def read_excel(path_to_file: str) -> pandas.DataFrame:
+    """Возвращает содержимое Excel - файла"""
     return pd.read_excel(path_to_file)
 
 
 def get_exchange_rate() -> list:
-    with open('user_settings.json', 'r') as json_file:
+    """Возвращает курс валют пользователя"""
+    with open("user_settings.json", "r") as json_file:
         currencies = json.load(json_file)
-    currency_rates = []
+    currency_rates: list = []
 
-    api_key = os.getenv('API_KEY_APILAYER')
+    api_key = os.getenv("API_KEY_APILAYER")
 
-    payload = {}
-    headers = {
-        "apikey": api_key
-    }
+    payload: dict = {}
+    headers = {"apikey": api_key}
 
     for currency in currencies['user_currencies']:
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount=1"
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
-        status_code = response.status_code
+        # status_code = response.status_code
         result = response.text
 
         json_to_list = json.loads(result)
@@ -130,12 +135,15 @@ def get_exchange_rate() -> list:
 
 
 def get_stock_prices() -> list:
-    with open('user_settings.json', 'r') as json_file:
+    """Возвращает стоимость акций пользователя"""
+    with open("user_settings.json", "r") as json_file:
         user_stocks = json.load(json_file)
 
-    api_key = os.getenv('API_KEY_FINNHUB')
+    stock_prices: list = []
+
+    api_key = os.getenv("API_KEY_FINNHUB")
     finnhub_client = finnhub.Client(api_key=api_key)
-    stock_prices = []
+
     for stock in user_stocks['user_stocks']:
         stock_prices.append({
             'stock': stock,
